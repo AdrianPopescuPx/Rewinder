0fe41a8bae92c6fa2ea557941982a00dd7d2375d
Adrian Popescu <adrianpopescupx@gmail.com>
2026-03-11 18:17:50 +0200

    create rewinder-service

---
+++ rewinder-service/src/main/java/com/rewinder/rewinder_service/RewinderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rewinder.rewinder_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewinderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewinderServiceApplication.class, args);
+	}
+
+}
---
+++ rewinder-service/src/test/java/com/rewinder/rewinder_service/RewinderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewinder.rewinder_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewinderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
